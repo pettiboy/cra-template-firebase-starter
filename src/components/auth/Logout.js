21464ca6f48b94da22c721181ada944c34279ba3
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-01-21 14:49:03 +0530

    ✨ NEW: init template code

---
+++ src/components/auth/Logout.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config/firebase";
+
+const Logout = ({ navigateTo = "/login" }) => {
+  const [disabled, setDisabled] = useState(false);
+  const navigate = useNavigate();
+  const logout = () => {
+    setDisabled(true);
+    signOut(auth)
+      .then(() => {
+        navigate(navigateTo);
+      })
+      .catch((error) => {
+        console.error(error);
+        setDisabled(false);
+      });
+  };
+
+  return (
+    <div>
+      <Button disabled={disabled} onClick={logout}>
+        Logout
+      </Button>
+    </div>
+  );
+};
+
+export default Logout;
